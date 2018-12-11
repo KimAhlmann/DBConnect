39f198eb66948ed084b8b27c78e7445a22937db1
Kim Ahlmann Pedersen <kimx457c@elev.tec.dk>
2018-12-11 12:08:21 +0100

    Add project files.

---
+++ DBConnect/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // opretter en kunde
+
+            string navn, adr, regNr, mærke, model, brændstofstype;
+
+            int årgang, km;
+
+            int tast;
+
+            Console.WriteLine("tast 1 for at oprette en kunde");
+            Console.WriteLine("tast 2 for at finde en kunde");
+            Console.WriteLine("tast 3 for at opdatere en kunde");
+            Console.WriteLine("tast 4 for at slette en kunde");
+
+            tast = int.Parse(Console.ReadLine());
+
+            switch (tast == 1)
+
+
+            {
+                case 1:
+
+                    Console.WriteLine("Indtast navn");
+                    navn = Console.ReadLine();
+                    Console.WriteLine("Indtast adresse");
+                    adr = Console.ReadLine();
+                    Console.WriteLine("Indtast regNr");
+                    regNr = Console.ReadLine(); ;
+                    Console.WriteLine("Indtast Mærke");
+                    mærke = Console.ReadLine();
+                    Console.WriteLine("Indtast Model");
+                    model = Console.ReadLine();
+                    Console.WriteLine("Indtast Brændstofstype");
+                    brændstofstype = Console.ReadLine();
+                    Console.WriteLine("årgang");
+                    årgang = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Indtast Kilometer");
+                    km = int.Parse(Console.ReadLine());
+
+                    break;
+
+                case 2:
+                    sqlDB.Select("select * from kunder");
+                    Console.ReadKey();
+
+                    break;
+
+                case 3:
+            }
+
+
+            opretKunde(navn, adr, regNr, mærke, model, brændstofstype, årgang, km);
+            sqlDB.Select("select * from kunder");
+            Console.ReadKey();
+        }
+
+
+        private static void opretKunde(string nv, string adr, string regNr, string mærke, string model, string brændstofstype, int årgang, int km)
+        {
+           // string statement = "insert into Kunder values('Knud Andersen', ' telegrafvej 9', 45)";
+
+            string statement = "insert into Kunder values('"+ nv +"' , '" + adr + "', '" + regNr + "' , '"+ mærke +"' , '"+ model +"', '"+ brændstofstype +"' , " + årgang +", "+ km +")";
+            sqlDB.Insert(statement);
+        }
+
+
+    }
+}
+
---
+++ DBConnect/sqlDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace DBConnect
+{
+
+    /// <summary>
+    /// space for ducomentation
+    /// </summary>
+    class sqlDB
+    {
+        //ConnectionString
+        private static string ConnectionString = @"Data Source=DESKTOP-4S7QIDF\SQLEXPRESS;Initial Catalog=sqleksempler; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void Insert(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void Select(string sql)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+                adapter.Fill(table);
+
+                foreach (DataRow kunde in table.Rows)
+                {
+                    Console.WriteLine(kunde["id"].ToString());
+                    Console.WriteLine(kunde["navn"].ToString());
+                    Console.WriteLine(kunde["adr"].ToString());
+                    Console.WriteLine(kunde["regNr"].ToString());
+                    Console.WriteLine(kunde["mærke"].ToString());
+                    Console.WriteLine(kunde["model"].ToString());
+                    Console.WriteLine(kunde["brændstofstype"].ToString());
+                    Console.WriteLine(kunde["årgang"].ToString());
+                    Console.WriteLine(kunde["km"].ToString());
+                   
+                    Console.WriteLine();
+
+                    
+                }
+                //string denførsterække = table.Rows[0]["navn"].ToString();
+                //Console.WriteLine(denførsterække);
+            }
+
+
+        }
+    }
+}
